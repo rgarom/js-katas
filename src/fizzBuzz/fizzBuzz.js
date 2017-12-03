44f28ba28e686061f86fc0587efc194fd3a34802
Rafa Garrido <rgarom@gmail.com>
2017-12-03 18:57:25 +0100

    Resolve FizzBuzz kata

--- src/fizzBuzz/fizzBuzz.js
+++ src/fizzBuzz/fizzBuzz.js
@@ -1,5 +1,21 @@
 function fizzBuzz(value) {
-  
+  const isMultipleOf3 = isMultipleOf(value, 3);
+  const isMultipleOf5 = isMultipleOf(value, 5);
+  const has3inValue = hasNumberInValue(value, 3);
+  const has5inValue = hasNumberInValue(value, 5);
+
+  if (isMultipleOf3 && isMultipleOf5 || has3inValue && has5inValue) return 'FizzBuzz';
+  else if (isMultipleOf3 || has3inValue) return 'Fizz';
+  else if (isMultipleOf5 || has5inValue) return 'Buzz';
+  else return value.toString();
+}
+
+function isMultipleOf(value, divider) {
+  return value % divider === 0;
+}
+
+function hasNumberInValue(value, number) {
+  return [...value.toString()].filter(n => n == number).length > 0;
 }
 
 export default fizzBuzz;
--- src/fizzBuzz/fizzBuzz.spec.js
+++ src/fizzBuzz/fizzBuzz.spec.js
@@ -1,9 +1,45 @@
 import fizzBuzz from "./fizzBuzz";
 
 describe("FizzBuzz spec", () => {
-  it("should return the value passed as String if it's not multiple of 3 or 5", () => {
+  it("should return the String value if the number is not multiple of 3 or 5", () => {
     const result = fizzBuzz(1);
 
     expect(result).to.equal("1");
+  });
+
+  it("should return Fizz if the value is multiple of 3", () => {
+    const result = fizzBuzz(3);
+
+    expect(result).to.equal("Fizz");
+  });
+
+  it("should return Buzz if the value is multiple of 5", () => {
+    const result = fizzBuzz(5);
+
+    expect(result).to.equal("Buzz");
+  });
+
+  it("should return FizzBuzz if the value is multiple of 3 and 5", () => {
+    const result = fizzBuzz(15);
+
+    expect(result).to.equal("FizzBuzz");
+  });
+
+  it("should return Fizz if the value has a 3 in it", () => {
+    const result = fizzBuzz(13);
+
+    expect(result).to.equal("Fizz");
+  });
+
+  it("should return Buzz if the value has a 5 in it", () => {
+    const result = fizzBuzz(52);
+
+    expect(result).to.equal("Buzz");
+  });
+
+  it("should return FizzBuzz if the value has a 3 and a 5 in it", () => {
+    const result = fizzBuzz(53);
+
+    expect(result).to.equal("FizzBuzz");
   });
 });
